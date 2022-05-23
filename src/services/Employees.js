b8d23e79873e4b1303b77eaef039607c8a3350b7
Yasar Altamirano <droidhp.ya@gmail.com>
2022-05-22 22:47:47 -0500

    add page employees, datatable, get and post employeee

---
+++ src/services/Employees.js
@@ -0,0 +1,20 @@
+const API_EMPLOYEES = 'https://6edeayi7ch.execute-api.us-east-1.amazonaws.com/v1/examen/employees/yasar_altamirano';
+
+export const getEmployees = async () => {
+    const result = await fetch(API_EMPLOYEES);
+    const employees = await result.json();
+    return employees;
+}
+
+export const saveEmployee = async employee => {
+    const result = await fetch(API_EMPLOYEES, {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(employee)
+    });
+    const emp = await result.json();
+    return emp;
+}
