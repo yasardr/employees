703670803ad0bf1287856cc1c3210d7f7976d594
Yasar Altamirano <droidhp.ya@gmail.com>
2022-05-23 00:55:56 -0500

    build

---
+++ build/precache-manifest.04007b077565730d106d049294447bec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "317935d6848e95bdc2c4",
+    "url": "/static/css/main.ef068923.chunk.css"
+  },
+  {
+    "revision": "317935d6848e95bdc2c4",
+    "url": "/static/js/main.317935d6.chunk.js"
+  },
+  {
+    "revision": "8abd614f29cc836bedfe",
+    "url": "/static/js/1.8abd614f.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ab2fb09f10107dac779c9f215aa63cb6",
+    "url": "/index.html"
+  }
+];
